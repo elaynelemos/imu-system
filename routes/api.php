e266167b04c980f9e231817da449f41a3e5679d2
larissa <larissa.almeida@discente.univasf.edu.br>
2022-04-03 13:08:39 -0300

    rotas de inserçao e update

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::middleware('api')->post('/records/store', 'RecordController@save');
+Route::middleware('api')->post('/index', 'RecordController@index');
+Route::middleware('api')->post('/records/update', 'RecordController@update');
+//Route::middleware('api')->post('/records/store', 'RecordController@store');
+
